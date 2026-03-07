8aaa74c33c45c9e781363ee6e7806d1c86442d3c
mehulagarwal13 <mehulagarwal333@gmail.com>
2026-03-07 13:28:42 +0530

    Fix routing one last time

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === 'production') {
     const clientPath = path.join(__dirname, '../client/dist');
     app.use(express.static(clientPath));
 
-    app.get('(.*)', (req, res) => {
+    app.get('/:any*', (req, res) => {
         if (!req.path.startsWith('/api')) {
             res.sendFile(path.join(clientPath, 'index.html'));
         } else {
@@ -55,7 +55,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // 404 handler for API routes
-app.use('/api/(.*)', (req, res) => res.status(404).json({ message: 'API route not found' }));
+app.use('/api/:any*', (req, res) => res.status(404).json({ message: 'API route not found' }));
 
 // 404 handler (general)
 app.use((req, res) => {
